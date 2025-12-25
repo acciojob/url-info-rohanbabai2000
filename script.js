5e3820d8d1304daf4b6ef127110988ac12213a39
ROHAN DAS <61402717+rohanbabai2000@users.noreply.github.com>
2025-12-25 23:22:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+var currentURL = window.location.href;
+
+    // Get the length of the URL
+var n = currentURL.length;
+
+    // Display the result using alert()
+alert("The length of the URL is: " + n);
